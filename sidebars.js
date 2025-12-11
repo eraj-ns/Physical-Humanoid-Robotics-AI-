9709868517a205c8f8e1a50eb4c0011a8bdcba2a
Eraj <ruksanasattar054@gmail.com>
2025-12-11 16:22:06 +0500

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,8 +1,6 @@
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
   tutorialSidebar: [
-
-    // ---------- MODULE 1 ----------
     {
       type: 'category',
       label: 'Module 1: ROS 2 Architecture & Programming',
@@ -12,11 +10,8 @@ const sidebars = {
         'Module1/ch02-nodes-topics-services',
         'Module1/ch03-python-agents-rclpy',
         'Module1/ch04-urdf-for-humanoids',
-        'Module1/module1',
       ],
     },
-
-    // ---------- MODULE 2 ----------
     {
       type: 'category',
       label: 'Module 2: Digital Twin and Simulation',
@@ -24,11 +19,8 @@ const sidebars = {
         'Module2/intro',
         'Module2/ch01-gazebo-physics-and-collisions',
         'Module2/ch02-digital-twin-environment-design',
-        'Module2/module2',
       ],
     },
-
-    // ---------- MODULE 3 ----------
     {
       type: 'category',
       label: 'Module 3: Isaac Sim and AI-Robot Basics',
@@ -36,11 +28,8 @@ const sidebars = {
         'Module3/Introduction-to-AI-Robot-Brain',
         'Module3/Isaac-Sim-Simulation',
         'Module3/Isaac-ROS-VSLAM-and-Nav2-Planning',
-        'Module3/module3',
       ],
     },
-
-    // ---------- MODULE 4 ----------
     {
       type: 'category',
       label: 'Module 4: Cognitive Planning and Autonomy',
@@ -52,8 +41,8 @@ const sidebars = {
         'Module4/ch04-capstone-autonomous-humanoid',
       ],
     },
-
   ],
 };
+
 
 module.exports = sidebars;
